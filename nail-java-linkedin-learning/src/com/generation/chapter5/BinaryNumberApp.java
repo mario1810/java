a7e337bd5fb08a4d0a79e21bbe7091e17ca95518
Mario Alvarez <mario.alvarez1810@gmail.com>
2022-08-29 17:54:21 -0500

    Matching Parentesis

--- nail-java-linkedin-learning/src/com/generation/chapter5/BinaryNumberApp.java
+++ nail-java-linkedin-learning/src/com/generation/chapter5/BinaryNumberApp.java
@@ -1,5 +1,57 @@
 package com.generation.chapter5;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryNumberApp {
+
+    public static void printBinary(int n) {
+        if (n <= 0) {
+            System.out.println("Nothing to print\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(Integer.toBinaryString(i));
+        }
+
+        System.out.println();
+    }
+
+    public static void printBinaryUsingQueue(int n) {
+        if (n <= 0) {
+            System.out.println("Nothing to print\n");
+            return;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+
+        for (int i = 1; i <= n; i++) {
+            Integer current = queue.remove();
+            System.out.println(current);
+
+            queue.add(current * 10);
+            queue.add(current * 10 + 1);
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+    	System.out.println("Conventional");
+        printBinary(6);
+        printBinary(-9);
+        printBinary(0);
+        printBinary(2);
+        printBinary(10);
+
+        System.out.println("Queue");
+        printBinaryUsingQueue(6);
+        printBinaryUsingQueue(-9);
+        printBinaryUsingQueue(0);
+        printBinaryUsingQueue(2);
+        printBinaryUsingQueue(10);
+    }
 
 }
--- nail-java-linkedin-learning/src/com/generation/chapter5/MatchingParenthesesApp.java
+++ nail-java-linkedin-learning/src/com/generation/chapter5/MatchingParenthesesApp.java
@@ -1,5 +1,99 @@
 package com.generation.chapter5;
 
-public class MatchingParenthesesApp {
+import java.util.Map;
+import java.util.Stack;
 
+
+public class MatchingParenthesesApp {
+	
+	 public static boolean matchingParentheses(String s) {
+	        Stack<Character> stack = new Stack<>();
+	        Map<Character, Character> blockSymbols = Map.of(')','(',
+	                ']', '[',
+	                '>','<');
+
+	        for (int i = 0; i < s.length(); i++) {
+	            char current = s.charAt(i);
+
+	            if (blockSymbols.containsValue(current)) {
+	                stack.push(current);
+	                continue;
+	            }
+
+	            if (blockSymbols.containsKey(current) &&
+	                    (stack.isEmpty() ||
+	                            blockSymbols.get(current) != stack.pop())) {
+	                return false;
+	            }
+	        }
+
+	        return stack.isEmpty();
+	    } 
+	 public static void main(String[] args) {
+	        String t1 = "(<[ ]>)";
+	        String t2 = "[<incre>ment]";
+	        String t3 = "<increment>";
+	        String t4 = "()incre<>ment<>[]";
+
+	        String t5 = "<increment(";
+	        String t6 = "[)incr]ement(";
+	        String t7 = "<i(ncr>e)ment";
+	        String t8 = "(<increment>";
+
+	        	
+	        System.out.println(matchingParenthesesM(t1));
+	        System.out.println(matchingParenthesesM(t2));
+	        System.out.println(matchingParenthesesM(t3));
+	        System.out.println(matchingParenthesesM(t4));
+
+	        System.out.println(matchingParenthesesM(t5));
+	        System.out.println(matchingParenthesesM(t6));
+	        System.out.println(matchingParenthesesM(t7));
+	        System.out.println(matchingParenthesesM(t8));
+	        
+	        System.out.println(matchingParentheses(t1));
+	        System.out.println(matchingParentheses(t2));
+	        System.out.println(matchingParentheses(t3));
+	        System.out.println(matchingParentheses(t4));
+
+	        System.out.println(matchingParentheses(t5));
+	        System.out.println(matchingParentheses(t6));
+	        System.out.println(matchingParentheses(t7));
+	        System.out.println(matchingParentheses(t8));
+	    }
+
+	private static boolean matchingParenthesesM(String text) {
+		Stack<Character> myStack = new Stack<>();
+		for(int i=0; i<text.length();i++) {
+			if(text.charAt(i)=='(' || text.charAt(i)=='[' || text.charAt(i)=='<'){
+				myStack.add(text.charAt(i));
+			}else if(text.charAt(i)==')'){
+				if(myStack.isEmpty()) {
+					return false;
+				}
+				Character saveChar=myStack.pop();
+				if(saveChar!='(')
+					return false;
+			}else if(text.charAt(i)==']'){				
+				if(myStack.isEmpty()) {
+					return false;
+				}
+				Character saveChar=myStack.pop();
+				if(saveChar!='[')
+					return false;
+			}else if(text.charAt(i)=='>'){				
+				if(myStack.isEmpty()) {
+					return false;
+				}
+				Character saveChar=myStack.pop();
+				if(saveChar!='<')
+					return false;
+			}
+		}
+	
+	if(myStack.isEmpty()){
+			return true;
+		}
+	return false;
+	}
 }
