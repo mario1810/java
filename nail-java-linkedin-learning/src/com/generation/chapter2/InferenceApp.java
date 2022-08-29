954fef93f8ee2366b630a9b0ee16f65cf3907f0d
Mario Alvarez <mario.alvarez1810@gmail.com>
2022-08-28 22:28:02 -0500

    var in java

---
+++ nail-java-linkedin-learning/src/com/generation/chapter2/InferenceApp.java
@@ -0,0 +1,13 @@
+package com.generation.chapter2;
+
+import java.util.List;
+
+public class InferenceApp {
+	public static void main(String[] args) {
+//        	List<String> orderedFoodItems = List.of("hotdog", "chips");
+//        	double total = 38.23;		
+			var orderedFoodItems = List.of("hotdog", "chips");
+			var total = 38.23;
+        
+    }
+}
