e19480edd12d6e1f9746826eb2874a71c721d135
Mario Alvarez <mario.alvarez1810@gmail.com>
2022-08-28 23:35:11 -0500

    Access data in a String#

---
+++ nail-java-linkedin-learning/src/com/generation/chapter3/AccessDataApp.java
@@ -0,0 +1,18 @@
+package com.generation.chapter3;
+
+public class AccessDataApp {
+	   public static void main(String[] args) {
+	        // 0 1 2 3 4 5
+	        // A P P L E S
+	        String apples = "Apples";
+	        char firstCharacter=apples.charAt(0);
+	        System.out.println(firstCharacter);
+	        System.out.println(apples.indexOf('e'));
+	        String sub=apples.substring(2,4);
+	        System.out.println(sub);
+	        System.out.println(apples.contains("app"));
+	        char[] applesArray = apples.toCharArray();
+	        int index = 4;
+	        System.out.println(applesArray[index]);
+	    }
+}
