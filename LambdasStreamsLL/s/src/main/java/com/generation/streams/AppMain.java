4c917de11523e8b664f1d5c892e737a2b6bdf599
Mario Alvarez <mario.alvarez1810@gmail.com>
2022-09-06 21:39:03 -0500

    collect operation

--- LambdasStreamsLL/s/src/main/java/com/generation/streams/AppMain.java
+++ LambdasStreamsLL/s/src/main/java/com/generation/streams/AppMain.java
@@ -3,6 +3,7 @@ package com.generation.streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class AppMain {
@@ -30,6 +31,15 @@ public class AppMain {
 							.map(item->item.toUpperCase())
 							.filter(item->item.startsWith("P"))
 							.forEach(item->System.out.println(item));
+		
+		//This throws a exception because Streams can only be used one time.
+		//shoppingListStream.forEach(item->System.out.println(item));
+		System.out.println(shoppingList);
+		
+		List<String> sortedShoppingList=shoppingList.stream()
+				.sorted()
+				.collect(Collectors.toList());
+		System.out.println(sortedShoppingList);
 
 	}
 
