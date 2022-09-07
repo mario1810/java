ab09af043b7b27cf0fcc67328bc9f634ed0dc671
Mario Alvarez <mario.alvarez1810@gmail.com>
2022-09-06 20:57:14 -0500

    test Challenge

--- LambdasStreamsLL/s/src/main/java/com/generation/EmployeeUtils.java
+++ LambdasStreamsLL/s/src/main/java/com/generation/EmployeeUtils.java
@@ -4,28 +4,30 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EmployeeUtils {
+	
 	  public String getFullName(Employee employee) {
-		    Function<Employee, String> fullName = e -> e.getFirstName() + " " + e.getLastName();
+		  			//<inputType,OutputType>
+		    Function<Employee, String> fullName = (Employee e) -> e.getFirstName() + " " + e.getLastName();
 		    return fullName.apply(employee);
 		  }
 
 		  public String getManagersLastName(Employee employee) {
-		    Function<Employee, String> managersLastName = null;
+		    Function<Employee, String> managersLastName = (Employee a)->a.getManager().getLastName();
 		    return managersLastName.apply(employee);
 		  }
 
 		  public boolean hasBeenEmployedLongerThanFiveYears(Employee employee) {
-		    Predicate<Employee> employedLongerThanFiveYears = null;
+		    Predicate<Employee> employedLongerThanFiveYears =(Employee a)->a.getYearsOfService()>5;
 		    return employedLongerThanFiveYears.test(employee);
 		  }
 
 		  public boolean hasMoreThanThreeDirectReports(Employee employee) {
-		    Predicate<Employee> moreThanThreeDirectReports = null;
+		    Predicate<Employee> moreThanThreeDirectReports = (Employee a)->a.getNumberOfDirectReports()>3;
 		    return moreThanThreeDirectReports.test(employee);
 		  }
 
 		  public boolean hasMoreThanThreeYearsOfServiceAndLessThanTwoDirectReports(Employee employee) {
-		    Predicate<Employee> moreThanThreeYearsOfServiceAndLessThanTwoDirectReports = null;
+		    Predicate<Employee> moreThanThreeYearsOfServiceAndLessThanTwoDirectReports = (Employee a)->a.getNumberOfDirectReports()<2&&a.getYearsOfService()>3;
 		    return moreThanThreeYearsOfServiceAndLessThanTwoDirectReports.test(employee);
 		  }
 }
